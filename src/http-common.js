a9c305ea4b846122fbb671cafbb03e21908e2aa0
Renato Viegas <renato.viegas@softplan.com.br>
2020-09-05 16:41:15 -0300

    Comitando configuração do front

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.HOST_SERVICE || 'http://localhost:3005/';
+const baseURL = process.env.HOST_SERVICE || 'https://rmv-grades-api.herokuapp.com/';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
